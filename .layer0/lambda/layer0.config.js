776d74bc47e46fa2281d39e7ac720ba690ffacbe
Camilla Bontogon <camillabontogon@Camillas-MacBook-Pro.local>
2022-01-28 15:23:17 +0800

    installed node and layer0

---
+++ .layer0/lambda/layer0.config.js
@@ -0,0 +1,2 @@
+module.exports={backends:{origin:{domainOrIp:"www.aimsindustrial.com.au",hostHeader:"www.aimsindustrial.com.au"}},name:"aimsindustrial.com.au"};
+//# sourceMappingURL=layer0.config.js.map
